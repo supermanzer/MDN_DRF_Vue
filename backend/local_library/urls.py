b6e96a5aca40014c0f71d3da6c3bf285e0dfd20c
Ryan <ryan.manzer@gmail.com>
2020-07-05 14:37:39 -0700

    Removing API related code, setting project state back to MDN Tutorial Part 5

--- backend/local_library/urls.py
+++ backend/local_library/urls.py
@@ -18,25 +18,9 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from rest_framework import routers
-from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
-
-from catalog.urls import router as cat_router
-
-from custom_auth.views import get_token
-
-
-router = routers.DefaultRouter()
-# REGISTER  ROUTES HERE
-router.registry.extend(cat_router.registry)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('get-token/', get_token),
-    path('auth/obtain_token', obtain_jwt_token),
-    path('auth/refresh_token', refresh_jwt_token),
 ]
 
 
